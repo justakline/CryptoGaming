0d0dfdabce90849924c6460e6840364a6736eaa8
Matt Dibbern <mdibbe1@students.towson.edu>
2023-10-28 18:12:56 -0400

    started to add the layout of my game and the css that goes with it

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import './assets/colorGuessStylesheet.css'
 
 function App() {
   return (
---
+++ client/src/games/ColorGuess.js
@@ -0,0 +1,13 @@
+import '../assets/ColorGuessStylesheet.css'
+
+const ColorGuess = () => {
+    return(
+        <div className="color-guess-container">
+            <button className="color-guess-btn">RED</button>
+            <button className="color-guess-btn">BLUE</button>
+            <button className="color-guess-btn">GREEN</button>
+        </div>
+    )
+}
+
+export default ColorGuess;
