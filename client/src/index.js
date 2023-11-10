c28512b1283868d85d3628614bfebc10ea930ee8
Arjun-Singh2001 <arjun.singh.m90@gmail.com>
2023-11-10 16:38:19 -0500

    TictacToe startup game design

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import TicTacToe from './games/TicTacToe';
 import ColorGuess from './games/ColorGuess';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-    <ColorGuess />
-);
+    <div>
+      <ColorGuess />
+      <TicTacToe />
+    </div>
+  );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
