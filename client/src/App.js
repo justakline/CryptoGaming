ad27e0ae65a7753bb2e2a7c0cb1c5663aada34b1
Matt Dibbern <mdibbern14@gmail.com>
2023-11-10 23:25:28 -0500

    commiting

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import TicTacToe from './games/TicTacToe';  // Import the TicTacToe component
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Tic-Tac-Toe Game</h1>
-        <TicTacToe />  
-      </header>
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <h1>Tic-Tac-Toe Game</h1>
+//         <TicTacToe />  
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
 
